7e52ffb6ddd7ace5463ab440539c37c10d163d8c
Samuel wanza <s.wanza@alustudent.com>
2023-07-15 17:59:30 +0200

    Added a fetch data functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Calculator from './components/Calculator';
 import './App.css';
+import Quotes from './components/Quotes';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Quotes />
     </div>
   );
 }
